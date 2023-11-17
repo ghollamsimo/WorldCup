c8fbd767cd16860f678d361a6ce58060fa7f57a7
ghollamsimo <ghollamsimo1@gmail.com>
2023-11-17 23:42:38 +0100

    Put The project Insert github

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-
-<?php include './temp/connect.php';
-
-
+<?php
+include './temp/connect.php';
 
 $Groupe = "SELECT * FROM Groups"; 
 $all_Groups = $conn->query($Groupe);
-
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -21,47 +15,53 @@ $all_Groups = $conn->query($Groupe);
     <title>Fifa WorldCup | 2022</title>
 </head>
 <body>
-  <header>
-<nav class="navbar navbar-light bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M417.3 360.1l-71.6-4.8c-5.2-.3-10.3 1.1-14.5 4.2s-7.2 7.4-8.4 12.5l-17.6 69.6C289.5 445.8 273 448 256 448s-33.5-2.2-49.2-6.4L189.2 372c-1.3-5-4.3-9.4-8.4-12.5s-9.3-4.5-14.5-4.2l-71.6 4.8c-17.6-27.2-28.5-59.2-30.4-93.6L125 228.3c4.4-2.8 7.6-7 9.2-11.9s1.4-10.2-.5-15l-26.7-66.6C128 109.2 155.3 89 186.7 76.9l55.2 46c4 3.3 9 5.1 14.1 5.1s10.2-1.8 14.1-5.1l55.2-46c31.3 12.1 58.7 32.3 79.6 57.9l-26.7 66.6c-1.9 4.8-2.1 10.1-.5 15s4.9 9.1 9.2 11.9l60.7 38.2c-1.9 34.4-12.8 66.4-30.4 93.6zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm14.1-325.7c-8.4-6.1-19.8-6.1-28.2 0L194 221c-8.4 6.1-11.9 16.9-8.7 26.8l18.3 56.3c3.2 9.9 12.4 16.6 22.8 16.6h59.2c10.4 0 19.6-6.7 22.8-16.6l18.3-56.3c3.2-9.9-.3-20.7-8.7-26.8l-47.9-34.8z"/></svg>
-    </a>
-  </div>
-</nav>
-  <div class="d-flex">
-    <h1 class="">Footbal For The Wider Community Of PlyMouth </h1>
-  </div>
-</header>
-
-<section class="cart">
-<?php
-   while ($row2 = mysqli_fetch_assoc($all_Groups)) :
-    $groupID = $row2["GroupID"];
-    $sql = "SELECT * FROM Teams WHERE id_fr = $groupID";
-    $all_Teams = $conn->query($sql);
-    ?>
-    <div>
-    <h1 class="fw-bold"><?= $row2["GroupName"] ?>
-    </div>
-    <main class="card-d ">
-    <?php
-     while ($row = mysqli_fetch_assoc($all_Teams)) :
-        ?>
-        <div >
-        <img class="cart_img" src="<?= $row["Flag"]?>" alt="" >
-        <button class="button" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row["TeamID"] ?>">See Details</button>
+    <header>
+        <nav class="navbar navbar-light bg-dark">
+            <div class="container">
+                <a class="navbar-brand text-light" href="#">
+                    FIFA
+                </a>
+            </div>
+        </nav>
+        <div class="d-flex">
+            <h1 class="text-center">FIFA WORLD CUP MOROCCO 2030</h1>
         </div>
+        <?php
+        $i = "SELECT * FROM Groups"; 
+        $k = $conn->query($i);
+        while ($g = mysqli_fetch_assoc($k)){
+          echo '<a href="?groupid='.$g["GroupID"].'" class="filter group-filter mx-2 ">'.$g["GroupName"].'</a>';
+        }
 
-        <div class="modal fade" id="exampleModal<?= $row["TeamID"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?= $row["TeamID"] ?>" aria-hidden="true">
+        $gid = isset($_GET["groupid"]) ? $_GET["groupid"] : null;
+        if($gid != null) {
+          $n = "SELECT * FROM Teams WHERE id_fr = $gid";
+          $m = $conn->query($n);
+          $u = mysqli_fetch_assoc($m);
+        }
+        ?>
+    </header>
+
+    <section class="cart">
+      <?php if(isset($u) && count($u) > 0): ?>
+        <main class="card-d ">
+                <?php
+                while ($row = mysqli_fetch_assoc($m)) :
+                ?>
+                <div class="card mx-3 gap-2 team" data-group="">
+                    <img class="cart_img" src="<?= $row["Flag"] ?>" alt="">
+                    <button class="button btn-secondary border-0" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row["TeamID"] ?>">See Details</button>
+                </div>
+                <!-- Modal code remains unchanged -->
+                              <!-- Modal code remains unchanged -->
+                              <div class="modal fade" id="exampleModal<?= $row["TeamID"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?= $row["TeamID"] ?>" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title fst-italic fw-bold" id="exampleModalLabel"><?= $row["TeamName"]?></h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close btn-light text-light" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <p>Team Id : <?= $row["TeamID"]?></p>
+      <div class="modal-body">  
         <p>Best Player : <?= $row["JoueurCle"]?></p>
         <p>Numbers Of Mtaches : <?= $row["Nbr_Match"]?></p>
         <p>Win : <?= $row["Victiore"]?></p>
@@ -69,6 +69,7 @@ $all_Groups = $conn->query($Groupe);
         <p>Defeat : <?= $row["Defaite"]?></p>
         <p>Capital : <?= $row["Capital"]?></p>
         <p>Regional population: <?= $row["Poplation"]?></p>
+        <p>Continent : <?= $row["Continent"]?></p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -76,12 +77,76 @@ $all_Groups = $conn->query($Groupe);
     </div>
   </div>
 </div>
-    <?php endwhile; ?>
-    </main>
-   
-  <?php endwhile; ?>
-  </section>
-  
+                <?php endwhile; ?>
+        </main>
+      <?php else:?>
+        <?php
+        while ($row2 = mysqli_fetch_assoc($all_Groups)) :
+    
+            $groupID = $row2["GroupID"];
+            $sql = "SELECT * FROM Teams WHERE id_fr = $groupID";
+            $all_Teams = $conn->query($sql);
+        ?>
+        <div class="group-container" data-group="<?= $row2["GroupName"] ?>">
+            <h1 class="fw-bold"><?= $row2["GroupName"] ?></h1>
+            <main class="card-d">
+                <?php
+                while ($row = mysqli_fetch_assoc($all_Teams)) :
+                ?>
+                <div class="card mx-3 gap-2 team" data-group="<?= $row2["GroupName"] ?>">
+                    <img class="cart_img" src="<?= $row["Flag"] ?>" alt="">
+                    <button class="button btn-secondary border-0" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $row["TeamID"] ?>">See Details</button>
+                </div>
+                <!-- Modal code remains unchanged -->
+                <div class="modal fade" id="exampleModal<?= $row["TeamID"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel<?= $row["TeamID"] ?>" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title fst-italic fw-bold" id="exampleModalLabel"><?= $row["TeamName"]?></h5>
+        <button type="button" class="btn-close btn-light text-light" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">  
+        <p>Best Player : <?= $row["JoueurCle"]?></p>
+        <p>Numbers Of Mtaches : <?= $row["Nbr_Match"]?></p>
+        <p>Win : <?= $row["Victiore"]?></p>
+        <p>Null : <?= $row["Nul"]?></p>
+        <p>Defeat : <?= $row["Defaite"]?></p>
+        <p>Capital : <?= $row["Capital"]?></p>
+        <p>Regional population: <?= $row["Poplation"]?></p>
+        <p>Continent : <?= $row["Continent"]?></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+                <?php endwhile; ?>
+            </main>
+        </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+    // document.querySelectorAll('.group-filter').forEach(function (button) {
+    //     button.addEventListener('click', function () {
+    //         const selectedGroup = this.dataset.group;
+    //         document.querySelectorAll('.group-container').forEach(function (container) {
+    //             const group = container.dataset.group;
+    //             if (group === selectedGroup) {
+    //                 container.style.display = 'block';
+    //             } else {
+    //                 container.style.display = 'none';
+    //             }
+    //         });
+    //     });
+
+        // Initially, show teams for the first group
+        // document.querySelector('.group-container').style.display = 'block';
+    // });
+</script>
 </body>
 </html>
